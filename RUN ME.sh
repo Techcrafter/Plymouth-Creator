4ca2a244309798382771594f6e3b8f527b347be0
Techcrafter <techcrafter808@outlook.com>
2020-11-29 12:54:54 +0100

    Fixed launch-scripts to be compatible with Linux Mint 20.

--- RUN ME.sh
+++ RUN ME.sh
@@ -3,11 +3,11 @@
 echo Please enter your sudo password if you are prompted to do so.
 
 echo Installing needed packages using apt...
-sudo apt install python3 python-tk ffmpeg
+sudo apt install python2 python-tk ffmpeg
 echo Done!
 
 echo Running program...
-python plymouth-creator.py
+python2 plymouth-creator.py
 echo Done!
 
 echo Have a nice day!
--- debug.sh
+++ debug.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Running "plymouth-creator.py"...
-python plymouth-creator.py
+python2 plymouth-creator.py
 echo Done!
 
 echo Waiting...
