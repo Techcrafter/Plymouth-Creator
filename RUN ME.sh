26b10d869851fe38aa6481f33abe038a7a4f35e3
DarulFebri <70104102+devrix123@users.noreply.github.com>
2024-05-02 14:20:23 +0700

    Update RUN ME.sh
    Some customizations to make support for python3.x versions

--- RUN ME.sh
+++ RUN ME.sh
@@ -3,11 +3,12 @@
 echo Please enter your sudo password if you are prompted to do so!
 
 echo Installing dependencies using apt...
-sudo apt install python2 python-gi python-tk ffmpeg
+sudo apt install python3 ffmpeg
+python3 -m pip install PyGObject tk 
 echo Done!
 
 echo Running program...
-python2 plymouth-creator.py
+python3 plymouth-creator.py
 echo Done!
 
 echo Have a nice day!
